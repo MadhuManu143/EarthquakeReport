9f16db7b9405b3c6bbb5cf5e32934b55a07e9a88
Madhu Rimmalapudi <madhu.rimmalapudi@gmail.com>
2019-02-14 15:38:37 -0800

    Initial Commit

---
+++ EarthquakeReport/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EarthquakeReport
+//
+//  Created by Madhu Rimmalapudi on 2/14/19.
+//  Copyright © 2019 Madhu Rimmalapudi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
